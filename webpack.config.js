c12bfc7a0e6d1fee25dc88c52ae667a52fbfb24d
vmik7 <vmik7@yandex.ru>
2021-06-30 13:38:36 +0300

    Add custom plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const UnusedJSPlugin = require('wp-plugin-unused-js');
 
 module.exports = {
     mode: 'development',
@@ -7,4 +8,5 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [new UnusedJSPlugin()],
 };
